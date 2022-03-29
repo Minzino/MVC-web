e1e83d7f692cbabbad965e1b4be61cdefabbf9af
Meenzino <86910955+Minzino@users.noreply.github.com>
2022-03-29 22:42:38 +0900

    addItemV2 - 상품 등록 처리 - ModelAttribute
    Contents
    - @ModelAttribute - 요청 파라미터 처리
    - @ModelAttribute 는 Item 객체를 생성하고, 요청 파라미터의 값을 프로퍼티 접근법(setXxx)으로 입력해준다.
    - @ModelAttribute - Model 추가
    - @ModelAttribute 는 중요한 한가지 기능이 더 있는데, 바로 모델(Model)에 @ModelAttribute 로 지정한 객체를 자동으로 넣어준다. 지금 코드를 보면 model.addAttribute("item", item) 가 주석처리
    되어 있어도 잘 동작하는 것을 확인할 수 있다.

--- src/main/java/hello/itemservice/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/basic/BasicItemController.java
@@ -19,33 +19,33 @@ public class BasicItemController {
     private final ItemRepository itemRepository;
 
     @GetMapping
-    public String items(Model model){
+    public String items(Model model) {
         List<Item> items = itemRepository.findAll();
-        model.addAttribute("items",items);
+        model.addAttribute("items", items);
         return "basic/items";
     }
 
     @GetMapping("/{itemId}")
-    public String item(@PathVariable long itemId, Model model){
+    public String item(@PathVariable long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
-        model.addAttribute("item",item);
+        model.addAttribute("item", item);
         return "basic/item";
     }
 
     /**
      * http Method로 기능을 구분
      * 같은 url이 오더라도 get요청이면 addForm으로 post요청이면 save 호출
-      */
+     */
     @GetMapping("/add")
-    public String addForm(){
+    public String addForm() {
         return "basic/addForm";
     }
 
-    @PostMapping("/add")
-    public String save(@RequestParam String itemName,
-                       @RequestParam int price,
-                       @RequestParam Integer quantity,
-                       Model model){
+    //    @PostMapping("/add")
+    public String addItemv1(@RequestParam String itemName,
+                            @RequestParam int price,
+                            @RequestParam Integer quantity,
+                            Model model) {
         Item item = new Item();
         item.setItemName(itemName);
         item.setPrice(price);
@@ -58,11 +58,20 @@ public class BasicItemController {
         return "basic/item";
     }
 
+    @PostMapping("/add")
+    public String addItemv2(@ModelAttribute("item") Item item, Model model) {
+
+        itemRepository.save(item);
+        model.addAttribute("item", item);
+
+        return "basic/item";
+    }
+
     /**
      * 테스트용 데이터 추가
      */
     @PostConstruct
-    public void init(){
+    public void init() {
         itemRepository.save(new Item("ItemA", 100000000, 10));
         itemRepository.save(new Item("ItemB", 200000000, 20));
         itemRepository.save(new Item("ItemC", 900000000, 20));
